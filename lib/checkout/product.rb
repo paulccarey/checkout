a8c534ba45bab5365cb392622a04bbf19acb773c
Paul Carey <paulccarey@gmail.com>
2020-10-01 22:03:58 +0100

    transform input keys to symbols on Product

--- lib/checkout/product.rb
+++ lib/checkout/product.rb
@@ -2,6 +2,8 @@ require 'dry-struct'
 
 module Checkout
   class Product < Dry::Struct
+    transform_keys(&:to_sym)
+    
     attribute :code, Types::String.constrained(format: /^\d{3}$/)
     attribute :name, Types::String
     attribute :price, Types::Money
--- spec/checkout/product_spec.rb
+++ spec/checkout/product_spec.rb
@@ -3,19 +3,22 @@ require 'rspec/dry/struct'
 
 module Checkout
   RSpec.describe Product do
-    describe 'attributes' do
-      subject { described_class }
+    subject { described_class }
 
-      it { is_expected.to have_attribute(:code, Types::Strict::String.constrained(format: /^\d{3}$/)) }
-
-      it 'only accepts three numeric digits for code' do
-        expect { described_class.new(code: '001', name: 'Lavender heart', price: '£9.25') }.to_not raise_error
-        expect { described_class.new(code: '0001', name: 'Lavender heart', price: '£9.25') }.to raise_error(Dry::Struct::Error)
-        expect { described_class.new(code: 'ABC', name: 'Lavender heart', price: '£9.25') }.to raise_error(Dry::Struct::Error)
-      end
-
-      it { is_expected.to have_attribute(:name, Types::Strict::String) }
-      it { is_expected.to have_attribute(:price, Types::Money) }  
+    it 'accepts hash keys as strings or symbols' do
+      expect { described_class.new('code': '001', 'name': 'Lavender heart', 'price': '£9.25') }.to_not raise_error
+      expect { described_class.new(code: '001', name: 'Lavender heart', price: '£9.25') }.to_not raise_error
     end
+
+    it { is_expected.to have_attribute(:code, Types::Strict::String.constrained(format: /^\d{3}$/)) }
+
+    it 'only accepts three numeric digits for code' do
+      expect { described_class.new(code: '001', name: 'Lavender heart', price: '£9.25') }.to_not raise_error
+      expect { described_class.new(code: '0001', name: 'Lavender heart', price: '£9.25') }.to raise_error(Dry::Struct::Error)
+      expect { described_class.new(code: 'ABC', name: 'Lavender heart', price: '£9.25') }.to raise_error(Dry::Struct::Error)
+    end
+
+    it { is_expected.to have_attribute(:name, Types::Strict::String) }
+    it { is_expected.to have_attribute(:price, Types::Money) }
   end
 end
