bb8b3676430de0a39c48e3e847077d3ccba40f30
Paul Carey <paulccarey@gmail.com>
2020-10-02 12:24:49 +0100

    Implements Basket.scan

--- lib/checkout/basket.rb
+++ lib/checkout/basket.rb
@@ -5,6 +5,17 @@ module Checkout
   class Basket < Dry::Struct
     transform_keys(&:to_sym)
 
-    attribute :promotions, Types::Array.of(Types.Instance(Checkout::Promotions::PromotionBase))
+    attribute :promotions, Types::Array.of(Types.Instance(Checkout::Promotions::PromotionBase)).default([])
+    attribute :items, Types::Array.of(Types.Instance(Checkout::Product)).default([])
+
+    class ItemNotProductError < StandardError; end
+
+    def scan(product)
+      unless product.instance_of?(Checkout::Product)
+        raise ItemNotProductError, "Only items of type #{Checkout::Product.name} can be scanned"
+      end
+
+      items << product
+    end
   end
 end
--- spec/checkout/basket_spec.rb
+++ spec/checkout/basket_spec.rb
@@ -1,9 +1,34 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 module Checkout
   RSpec.describe Basket do
     subject { described_class }
-    
-    it { is_expected.to have_attribute(:promotions, Types::Array.of(Types.Instance(Checkout::Promotions::PromotionBase))) }
+
+    it {
+      is_expected.to have_attribute(
+        :promotions,
+        Types::Array.of(Types.Instance(Checkout::Promotions::PromotionBase)).default([])
+      )
+    }
+    it { is_expected.to have_attribute(:items, Types::Array.of(Types.Instance(Checkout::Product)).default([])) }
+
+    describe '#scan' do
+      let(:basket) { Checkout::Basket.new }
+
+      it 'adds the product to the basket items' do
+        product = Checkout::Product.new('code': '001', 'name': 'Lavender heart', 'price': '£9.25')
+        basket.scan(product)
+        expect(basket.items).to include(product)
+      end
+
+      it 'raises an error if the item is not a Product' do
+        expect { basket.scan(:product) }.to raise_error(
+          Checkout::Basket::ItemNotProductError,
+          "Only items of type #{Checkout::Product.name} can be scanned"
+        )
+      end
+    end
   end
 end
